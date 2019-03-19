38ffd6f85d21eb3b778018c5f2c7c5e8f6fb0b94
Dibasic <Dibasic@users.noreply.github.com>
2019-03-19 15:12:52 -0500

    Replace webserver hack with 'real' server

--- server.py
+++ server.py
@@ -2,4 +2,12 @@ from config import *
 from flask import Flask
 
 app = Flask(__name__)
-app.run(HOST, PORT)
+
+@app.route('/')
+def index():
+    json_data = {'Hello': 'World!'}
+    return jsonify(json_data)
+
+
+if __name__ == '__main__':
+app.run()
